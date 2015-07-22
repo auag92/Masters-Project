1760c3c6116d89e377ee426de3b515c874dd039e
apaar <apaar92@gmail.com>
2015-07-22 21:53:44 +0530

    almsot there

--- isotropic_growth_fluid_flow/code/mpi/gauss_siedel_mpi.c
+++ isotropic_growth_fluid_flow/code/mpi/gauss_siedel_mpi.c
@@ -4,11 +4,14 @@
 #define LTAG  777
 #define RTAG  666
 #define WRITE 555
+#define ERROR 888
+#define BREAK 111
 
-int offset_ax; offset_ay;
-double error;
+//int offset_ax; offset_ay;
+double error, err;
 double tol=1.0e-6;
 int iter  = 0;
+int flag = 0;
 
 gs_mpi(double *P, double *fn, double *a_x, double *a_y){
 
@@ -44,13 +47,25 @@ gs_mpi(double *P, double *fn, double *a_x, double *a_y){
       // MPI_Send(&offset_ay,         1,                   MPI_INT,         dest,   BEGIN,  MPI_COMM_WORLD);
       // MPI_Send(&rows_ay,           1,                   MPI_INT,         dest,   BEGIN,  MPI_COMM_WORLD);
       // MPI_Send(&a_y[offset_ay],    (rows-1)*(pmesh-2),  MPI_DOUBLE,      dest,   BEGIN,  MPI_COMM_WORLD);
-
     }
-    for (t=1; t < ntimesteps; t++) {
-      if (t%saveT == 0) {
+    for (;;) {
+      for(rank = 1; rank <= numworkers; rank ++){
+        MPI_Recv(&err,     1,     MPI_DOUBLE,    rank,   ERROR,  MPI_COMM_WORLD, &status);
+        error  += err;
+      }
+      if (error < tol){
+        flag = 1;
+        for(rank = 1; rank <= numworkers; rank ++){
+          MPI_Send(flag,     1,     MPI_INT,    rank,   BREAK,  MPI_COMM_WORLD);
+        }
         receivefrmworker();
-        if(check_error())
-          break;
+        break;
+      }
+      else{
+        flag = 0;
+        for(rank = 1; rank <= numworkers; rank ++){
+          MPI_Send(flag,     1,     MPI_INT,    rank,   BREAK,  MPI_COMM_WORLD);
+        }
       }
     }
   }
@@ -81,16 +96,26 @@ gs_mpi(double *P, double *fn, double *a_x, double *a_y){
       end = rows;
     }
 
-    boundary_pressure(taskid);
-    red_solver();
-    mpiexchange();
-    black_solver();
-    sendtomaster();
+    for (; ;) {
+      boundary_pressure(taskid);
+      mpi_exchange();
+      red_solver();
+      mpiexchange();
+      black_solver();
+      error = compute_error();
+
+      MPI_Send(&error,     1,     MPI_DOUBLE,    MASTER,   ERROR,  MPI_COMM_WORLD);
+      MPI_Recv(flag,       1,     MPI_INT,       MASTER,   BREAK,  MPI_COMM_WORLD,&status);
+
+      if (flag == 1){
+        sendtomaster();
+      }
+    }
   }
 }
 void red_solver(){
   int i, j, indx, indx_rght, indx_frnt, indx_lft, indx_bck, indx_ax, indx_ay;
-  for(i=1; i < rows-1; i++) {
+  for(i=start; i <= end; i++) {
     for(j=1;j < pmesh-1;j++) {
       indx         = i*pmesh      + j;
       indx_rght    = indx         + 1;
@@ -101,16 +126,21 @@ void red_solver(){
       indx_ay      = (i-1)*(pmesh-2)  +(j-1);
 
       if (((i+j)%2) == 0) {
-        P[indx]  = -1.0*(a_x[indx_ax]*P[indx_lft] + a_x[indx_ax+1]*P[indx_rght]
-        + a_y[indx_ay]*P[indx_bck] + a_y[indx_ay+(pmesh-2)]*P[indx_frnt]) + deltax*deltax*fn[indx];
-        P[indx] /= -1.0*(a_x[indx_ax+1]+a_x[indx_ax]+a_y[indx_ay+(pmesh-2)]+a_y[indx_ay]);
+        P[indx]  = -1.0*(P[indx_lft] + P[indx_rght]
+        + P[indx_bck] + P[indx_frnt]) + deltax*deltax*fn[indx];
+        P[indx] /= -1.0*4.0;
       }
+      // if (((i+j)%2) == 0) {
+      //   P[indx]  = -1.0*(a_x[indx_ax]*P[indx_lft] + a_x[indx_ax+1]*P[indx_rght]
+      //   + a_y[indx_ay]*P[indx_bck] + a_y[indx_ay+(pmesh-2)]*P[indx_frnt]) + deltax*deltax*fn[indx];
+      //   P[indx] /= -1.0*(a_x[indx_ax+1]+a_x[indx_ax]+a_y[indx_ay+(pmesh-2)]+a_y[indx_ay]);
+      // }
     }
   }
 }
 void black_solver(int rows){
   int i, j, indx, indx_rght, indx_frnt, indx_lft, indx_bck, indx_ax, indx_ay;
-  for(i=1; i < rows-1; i++) {
+  for(i=start; i <= end; i++) {
     for(j=1;j < pmesh-1;j++) {
       indx         = i*pmesh      + j;
       indx_rght    = indx         + 1;
@@ -119,19 +149,24 @@ void black_solver(int rows){
       indx_bck     = indx         - pmesh;
       indx_ax      = (i-1)*(pmesh-1)  + (j-1);
       indx_ay      = (i-1)*(pmesh-2)  + (j-1);
-
-      if (((i+j)%2) != 0) {
-        P[indx]  =-1.0*(a_x[indx_ax]*P[indx_lft] + a_x[indx_ax+1]*P[indx_rght]
-        + a_y[indx_ay]*P[indx_bck] + a_y[indx_ay+(pmesh-2)]*P[indx_frnt]) + deltax*deltax*fn[indx];
-        P[indx] /= -1.0*(a_x[indx_ax]+a_x[indx_ax+1]+a_y[indx_ay]+a_y[indx_ay+(pmesh-2)]);
+      if (((i+j)%2) == 0) {
+        P[indx]  = -1.0*(P[indx_lft] + P[indx_rght]
+        + P[indx_bck] + P[indx_frnt]) + deltax*deltax*fn[indx];
+        P[indx] /= -1.0*4.0;
       }
+
+      // if (((i+j)%2) != 0) {
+      //   P[indx]  =-1.0*(a_x[indx_ax]*P[indx_lft] + a_x[indx_ax+1]*P[indx_rght]
+      //   + a_y[indx_ay]*P[indx_bck] + a_y[indx_ay+(pmesh-2)]*P[indx_frnt]) + deltax*deltax*fn[indx];
+      //   P[indx] /= -1.0*(a_x[indx_ax]+a_x[indx_ax+1]+a_y[indx_ay]+a_y[indx_ay+(pmesh-2)]);
+      // }
     }
   }
 }
 double compute_error(double *a_x, double *a_y, double *P, double *fn) {
   double error=0.0;
   int indx, indx_rght, indx_frnt, indx_lft, indx_bck, i, j,indx_ax,indx_ay;
-  for(i=1; i < pmesh-1; i++) {
+  for(i=start; i < end; i++) {
     for(j=1;j < pmesh-1; j++) {
       indx  = i*pmesh    + j;
       indx_rght         = indx      + 1;
@@ -141,10 +176,12 @@ double compute_error(double *a_x, double *a_y, double *P, double *fn) {
       indx_ax      = (i-1)*(pmesh-1)  + j-1;
       indx_ay      = (i-1)*(pmesh-2)  + j-1;
 
-      error += fabs(a_x[indx_ax]*P[indx_lft] + a_x[indx_ax+1]*P[indx_rght]
-      + a_y[indx_ay]*P[indx_bck] + a_y[indx_ay+(pmesh-2)]*P[indx_frnt]
-      - (a_x[indx_ax]+a_x[indx_ax+1]+a_y[indx_ay]+a_y[indx_ay+(pmesh-2)])*P[indx]
-      - deltax*deltax*fn[indx]);
+      error += fabs(P[indx_lft] + P[indx_rght]
+      + P[indx_bck] + P[indx_frnt] - (4.0)*P[indx] - deltax*deltax*fn[indx]);
+      // error += fabs(a_x[indx_ax]*P[indx_lft] + a_x[indx_ax+1]*P[indx_rght]
+      // + a_y[indx_ay]*P[indx_bck] + a_y[indx_ay+(pmesh-2)]*P[indx_frnt]
+      // - (a_x[indx_ax]+a_x[indx_ax+1]+a_y[indx_ay]+a_y[indx_ay+(pmesh-2)])*P[indx]
+      // - deltax*deltax*fn[indx]);
     }
   }
   return(error);
